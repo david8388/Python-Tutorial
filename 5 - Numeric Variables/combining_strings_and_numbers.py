b8b61bf5acfb12c9117e284b5e5921c74d4540c5
David <hsienwu830808@gmail.com>
2019-09-29 06:51:41 +0800

    Add numbers_treated_as_strings.py

---
+++ 5 - Numeric Variables/combining_strings_and_numbers.py
@@ -0,0 +1,11 @@
+days_in_feb = 28
+
+print(days_in_feb)
+
+# if you combine strings with numbers, Python get confused.
+# print(days_in_feb + ' days in February')
+
+# You need to convert the number to a string to display the value 
+# this line of code will work
+
+print(str(days_in_feb) + ' days in February')
---
+++ 5 - Numeric Variables/numbers_treated_as_strings.py
@@ -0,0 +1,5 @@
+# the input function always returns strings
+first_num = input('Enter first number ')
+second_num = input('Enter second number ')
+
+print(first_num + second_num)
