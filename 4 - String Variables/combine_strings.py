ab669baed3a669e121ac9052e5a31e9d98df8ab3
David <hsienwu830808@gmail.com>
2019-09-29 06:19:07 +0800

    Add custom string formatting in combine_strings.py

--- 4 - String Variables/combine_strings.py
+++ 4 - String Variables/combine_strings.py
@@ -3,4 +3,16 @@ last_name = 'Wu'
 
 print(first_name + last_name)
 
-print('Hello '+ first_name + ' '+ last_name)
+print('Hello, '+ first_name + ' '+ last_name)
+
+## custom string formatting
+
+output = 'Hello, {} {}'.format(first_name, last_name)
+print(output)
+
+output = 'Hello, {0} {1}'.format(first_name, last_name)
+print(output)
+
+## only available in Python3
+output = f'Hello, {first_name} {last_name}'
+print(output)
