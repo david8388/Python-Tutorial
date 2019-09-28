feabed36c17e1b0fca393add63f4e17f7b21bbc7
David <hsienwu830808@gmail.com>
2019-09-29 06:57:16 +0800

    Add convert_strings_to_numbers_for_math.py

---
+++ 5 - Numeric Variables/convert_strings_to_numbers_for_math.py
@@ -0,0 +1,13 @@
+first_num = input('Enter first number ')
+second_num = input('Enter second number ')
+
+# if you have a string variable containing a number
+# and you want to treat it as a number
+# you must convert it ot a numeric datatype
+# int() converts a string to an integer
+
+print(int(first_num) + int(second_num))
+
+# float() converts a string to an decimal or float number
+
+print(float(first_num) + float(second_num)) 
