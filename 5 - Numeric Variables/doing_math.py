64d71662c033dd344634833f8b6fe615582eb00b
David <hsienwu830808@gmail.com>
2019-09-29 06:39:27 +0800

    Add doing_math.py

---
+++ 5 - Numeric Variables/doing_math.py
@@ -0,0 +1,17 @@
+first_num = 6
+second_num = 2
+
+print('Addition')
+print(first_num + second_num)
+
+print('Subtraction')
+print(first_num - second_num)
+
+print('Multiplication')
+print(first_num * second_num)
+
+print('Division')
+print(first_num / second_num)
+
+print('Exponent')
+print(first_num ** second_num)
